eed195118f96f69c347aea6996837b0848b63b29
jeevathangavel <37336054+jeevathangavel@users.noreply.github.com>
2018-03-19 16:18:14 +0530

    Update larg among three.py

--- Beginner/larg among three.py
+++ Beginner/larg among three.py
@@ -3,8 +3,8 @@ a=int(a)
 b=int(b)
 c=int(c)
 if((a>=b) and (a>=c)):
-	print("a is large")
-elif((b>=a)and (b>=c)):
-	print("b is large")
+	print a
+elif((b>=a) and (b>=c)):
+	print b
 else:
-	print("c is large")
+	print c
